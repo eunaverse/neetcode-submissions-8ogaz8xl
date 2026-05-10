2af767f27c43d7c3a13b829e9a4a27dd41f2687f
eunhwa99 <68810660+eunhwa99@users.noreply.github.com>
2026-05-11 07:15:18 +0900

    Add: find-minimum-in-rotated-sorted-array - submission-1

---
+++ Data Structures & Algorithms/find-minimum-in-rotated-sorted-array/submission-1.java
@@ -0,0 +1,20 @@
+class Solution {
+        public int findMin(int[] nums) {
+                int left = 0;
+                        int right = nums.length - 1;
+
+                                int res = 1001;
+                                        while(left<=right){
+                                                    int mid = left + (right-left)/2;
+                                                                if(nums[mid] > nums[right]){
+                                                                                left = mid + 1; 
+                                                                                            }
+                                                                                                        else{
+                                                                                                                        right = mid - 1;
+                                                                                                                        res=Math.min(res, nums[mid]);
+                                                                                                                                    }
+                                                                                                                                
+                                                                                                                                                         }
+                                                                                                                                                                 return res;
+                                                                                                                                                                     }
+                                                                                                                                                                     }
